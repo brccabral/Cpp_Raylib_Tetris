7bdb860368e975a0b19bcb762c64fd3c53966d43
Bruno Cabral <cabral.brc@gmail.com>
2024-06-05 10:40:59 -0700

    test colors
    Signed-off-by: Bruno Cabral <cabral.brc@gmail.com>

--- main.cpp
+++ main.cpp
@@ -9,6 +9,9 @@ int main()
     constexpr int screenHeight = 600;
 
     Grid grid;
+    grid.grid[0][0] = 1;
+    grid.grid[3][5] = 4;
+    grid.grid[17][8] = 7;
 
     InitWindow(screenWidth, screenHeight, "Tetris");
     SetTargetFPS(60);

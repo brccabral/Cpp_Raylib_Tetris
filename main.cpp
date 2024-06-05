ad7f3d014905ce58f5197f0f748b4724ffed1815
Bruno Cabral <cabral.brc@gmail.com>
2024-06-05 10:02:17 -0700

    bouncing ball (start code)

--- main.cpp
+++ main.cpp
@@ -1,15 +1,27 @@
 #include <raylib.h>
+#include "ball.h"
 
 int main()
 {
-    InitWindow(800, 600, "Tetris");
+    constexpr Color darkGreen{20, 160, 133, 255};
+
+    constexpr int screenWidth = 800;
+    constexpr int screenHeight = 600;
+
+    Ball ball;
+
+    InitWindow(screenWidth, screenHeight, "My first RAYLIB program!");
+    SetTargetFPS(60);
+
     while (!WindowShouldClose())
     {
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("Tetris game!", 190, 200, 20, LIGHTGRAY);
+        ClearBackground(darkGreen);
+        ball.Update();
+        ball.Draw();
         EndDrawing();
     }
+
     CloseWindow();
     return 0;
 }

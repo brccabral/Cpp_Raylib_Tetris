6724b1e3d78d381a2eb3eb1f84475e9120807b0a
Bruno Cabral <cabral.brc@gmail.com>
2025-09-14 10:42:41 -0700

    build for emscripten
    Signed-off-by: Bruno Cabral <cabral.brc@gmail.com>

--- build_web.sh
+++ build_web.sh
@@ -2,6 +2,6 @@
 
 source $EMSDK/emsdk_env.sh
 
-emcc -o tetris.html block.cpp blocks.cpp colors.cpp game.cpp grid.cpp main.cpp position.cpp -Os -Wall $RAYLIB_INSTALL_PATH/lib/libraylib.a -I. -I$RAYLIB_INSTALL_PATH/include -L. -L$RAYLIB_INSTALL_PATH/lib -s USE_GLFW=3 -s ASYNCIFY --shell-file $RAYLIB_SRC_PATH/shell.html -DPLATFORM_WEB --preload-file resources/Sounds --preload-file resources/Font
+emcc -o tetris.html block.cpp blocks.cpp colors.cpp game.cpp grid.cpp main.cpp position.cpp -Os -Wall $RAYLIB_INSTALL_PATH/lib/libraylib.a -I. -I$RAYLIB_INSTALL_PATH/include -L. -L$RAYLIB_INSTALL_PATH/lib -s USE_GLFW=3 -s ASYNCIFY --shell-file $RAYLIB_SRC_PATH/shell.html -DPLATFORM_WEB --preload-file resources
 
 # python -m http.server 8080
